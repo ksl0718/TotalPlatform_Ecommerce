2b0bc32addc219488f95f51d80bb64d84c79033d
SEUNGRAEM <ksl0718@nate.com>
2024-10-25 17:34:01 +0900

    multi database 연동

--- pinguMarket/src/main/java/com/example/demo/Admin/Notice/AdminNoticeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Admin.Notice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Notice;
-
-public interface AdminNoticeRepository extends JpaRepository<Notice, Integer> {
-
-}
--- pinguMarket/src/main/java/com/example/demo/Image/test.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.Image;
-
-public class test {
-
-}
